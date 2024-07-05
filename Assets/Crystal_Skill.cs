4c5aa198fadc0889f29f281445c49797830265ef
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-05 18:06:57 +0500

    Explosive crystal

--- Assets/Crystal_Skill.cs
+++ Assets/Crystal_Skill.cs
@@ -7,6 +7,14 @@ public class Crystal_Skill : Skill
     [SerializeField] private float crystalDuration;
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
+
+    [Header("Explosive crystal")] 
+    [SerializeField] private bool canExplode;
+
+    [Header("Moving crystal")] 
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+    
     public override void UseSkill()
     {
         base.UseSkill();
@@ -16,12 +24,17 @@ public class Crystal_Skill : Skill
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
             Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
             
-            currentCrystalScript.SetupCrystal(crystalDuration);
+            currentCrystalScript.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
         else
         {
+            Vector2 playerPos = player.transform.position;
+
             player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+            
+            
+            currentCrystal.transform.position = playerPos;
+            currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
     }
 }
