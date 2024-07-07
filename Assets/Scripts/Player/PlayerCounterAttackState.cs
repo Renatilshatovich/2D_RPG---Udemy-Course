021a0fc8b466304a96d146d6b27da2bab9665c72
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-08 00:50:49 +0500

    Improving skills in a skill tree

--- Assets/Scripts/Player/PlayerCounterAttackState.cs
+++ Assets/Scripts/Player/PlayerCounterAttackState.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class PlayerCounterAttackState : PlayerState
 {
+    private bool canCreateClone;
+    
     public PlayerCounterAttackState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
     public override void Enter()
     {
         base.Enter();
-        
+
+        canCreateClone = true;
         stateTimer = player.counterAttackDuration;
         player.anim.SetBool("SuccessfulCounterAttack", false);
     }
@@ -29,6 +32,11 @@ public class PlayerCounterAttackState : PlayerState
                 {
                     stateTimer = 10;
                     player.anim.SetBool("SuccessfulCounterAttack", true);
+                    if (canCreateClone)
+                    {
+                        canCreateClone = false;
+                        player.skill.clone.CreateCloneOnCounterAttack(hit.transform);
+                    }
                 }
             }
         }
