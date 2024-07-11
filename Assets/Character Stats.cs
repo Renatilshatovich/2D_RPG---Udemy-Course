68a82d9e1613ec3a76ff114cea5a67b85dfa8c6a
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-11 19:22:34 +0500

    Stat Script

--- Assets/Character Stats.cs
+++ Assets/Character Stats.cs
@@ -1,24 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class CharacterStats : MonoBehaviour
 {
-     public int damage;
-     public int maxHealth;
-
+     public Stat damage;
+     public Stat maxHealth;
+     
      [SerializeField] private int currentHealth;
 
      private void Start()
      {
-          currentHealth = maxHealth;
+          currentHealth = maxHealth.GetValue();
+          
+          damage.AddModifier(4);
      }
 
      public void TakeDamage(int _damage)
      {
-          currentHealth -= damage;
+          currentHealth -= _damage;
 
           if (currentHealth < 0)
                Die();
@@ -26,6 +29,6 @@ public class CharacterStats : MonoBehaviour
 
      private void Die()
      {
-          
+          throw new NotImplementedException();
      }
 }
