3fddb3ead40e54d9c974d0d492fd28537c23b637
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-14 21:10:19 +0500

    Major Stats And Defensive Stats

--- Assets/Character Stats.cs
+++ Assets/Character Stats.cs
@@ -1,15 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class CharacterStats : MonoBehaviour
 {
-     public Stat strength;
-     public Stat damage;
+     [Header("Major stats")]
+     public Stat strength;    // point increase damage by 1 and crit. power by 1%
+     public Stat agility;     // 1 point increase evasion by 1% and crit. chance by 1%
+     public Stat intelligence;    // 1 point increase magic damage by 1 and magic resistance by 3
+     public Stat vitality;    // 1 point increase health by 3 or points
+     
+     [Header("Defensive stats")]
      public Stat maxHealth;
+     public Stat armor;
+     public Stat evasion;
+     
+     public Stat damage;
      
      [SerializeField] private int currentHealth;
 
@@ -20,11 +24,15 @@ public class CharacterStats : MonoBehaviour
 
      public virtual void DoDamage(CharacterStats _targetStats)
      {
+          if (TargetCanAvoidAttack(_targetStats))
+               return;
+
           int totalDamage = damage.GetValue() + strength.GetValue();
-          
+
+          totalDamage = CheckTargetArmor(_targetStats, totalDamage);
           _targetStats.TakeDamage(totalDamage);
      }
-
+     
      public virtual void TakeDamage(int _damage)
      {
           currentHealth -= _damage;
@@ -32,9 +40,26 @@ public class CharacterStats : MonoBehaviour
           if (currentHealth < 0 )
                Die();
      }
+     private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+     {
+          totalDamage -= _targetStats.armor.GetValue();
+          totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+          return totalDamage;
+     }
 
      protected virtual void Die()
      {
           // throw new NotImplementedException();
+     }
+     private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+     {
+          int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+          if (Random.Range(0,100 ) < totalEvasion)
+          {
+               return true;
+          }
+
+          return false;
      }
 }
