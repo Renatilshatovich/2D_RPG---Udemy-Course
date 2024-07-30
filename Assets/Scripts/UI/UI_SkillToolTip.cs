66a3358331a7c041144884adb76ddb201e97c3a5
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-30 16:22:17 +0500

    Skill Tree UI

---
+++ Assets/Scripts/UI/UI_SkillToolTip.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_SkillToolTip : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI skillText;
+    [SerializeField] private TextMeshProUGUI skillName;
+    
+    public void ShowToolTip(string _skillDescprtion,string _skillName)
+    {
+        skillName.text = _skillName;
+        skillText.text = _skillDescprtion;
+
+        gameObject.SetActive(true);
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+    }
+ 
+}
