a56a7a38bce692327d138fd944835a6e0241ebf2
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-07-31 11:32:13 +0500

    Merge Skill Tree with Parry Skill

--- Assets/Scripts/Skills/Clone_Skill.cs
+++ Assets/Scripts/Skills/Clone_Skill.cs
@@ -33,13 +33,12 @@ public class Clone_Skill : Skill
     }
     
 
-    public void CreateCloneOnCounterAttack(Transform _enemyTransform)
+    public void CreateCloneWithDelay(Transform _enemyTransform)
     {
-        if (canCreateCloneOnCounterAttack)
-            StartCoroutine(CreateCloneWithDelay(_enemyTransform, new Vector3(2 * player.facingDir, 0)));
+        StartCoroutine(CloneDelayCoroutine(_enemyTransform, new Vector3(2 * player.facingDir, 0)));
     }
 
-    private IEnumerator CreateCloneWithDelay(Transform _transform, Vector3 _offset)
+    private IEnumerator CloneDelayCoroutine(Transform _transform, Vector3 _offset)
     {
         yield return new WaitForSeconds(.4f);
         CreateClone(_transform, _offset);
