e47c702b07499f77031e1c3f1658724f92a3a595
Renat Husnetdinov <renatilshatovich@yandex.ru>
2024-08-03 23:09:38 +0500

    Save system

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviour, ISaveManager
 {
      public static PlayerManager instance;
      public Player player;
@@ -32,4 +32,13 @@ public class PlayerManager : MonoBehaviour
      }
 
      public int GetCurrency() => currency;
+     public void LoadData(GameData _data)
+     {
+          this.currency = _data.currency;
+     }
+
+     public void SaveData(ref GameData _data)
+     {
+          _data.currency = this.currency;
+     }
 }
